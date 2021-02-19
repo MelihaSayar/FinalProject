532e388dfccd73d79e3320c82613fcdf02d91de1
Meliha SAYAR <melihasayar6@gmail.com>
2021-02-19 15:11:22 +0300

    19/02/2021

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,15 +1,16 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //generic constraint - generic kısıt
-    //class : referans tip
+    //class : referans tip olabilir (T:class)
     //IEntity : IEntity olabilir veya IEntity imlemente eden bir nesne olabilir
     //new() : new'lenebilir olmalı
-    public interface IEntityRepository<T> where T:class, new()
+    public interface IEntityRepository<T> where T:class,IEntity,new()
     {
         List<T> GetAll(Expression<Func<T, bool>> filter=null);
         T Get(Expression<Func<T, bool>> filter);
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        //this = Result (Class'ı çalıştır)
+        //this(success) = İki parametreli constructer'ı çalıştır aynı zamanda tek parametreli constructer'ı da çalıştır
+        public Result(bool success, string message):this(success)  
+        {
+            Message = message;
+        }
+        //Overloading
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; } //getter Readonly'dir. Readonly'ler constructer'da set edilebilir
+    }
+}
