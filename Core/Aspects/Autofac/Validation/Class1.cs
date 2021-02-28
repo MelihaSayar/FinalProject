d16e997f8aedeb777faf5adf793e364ff12ce30b
Meliha SAYAR <melihasayar6@gmail.com>
2021-03-01 01:38:21 +0300

    1 Mart 2021

--- Core/Aspects/Autofac/Validation/Class1.cs
+++ Core/Aspects/Autofac/Validation/Class1.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Interceptors;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Core.Aspects.Autofac.Validation
