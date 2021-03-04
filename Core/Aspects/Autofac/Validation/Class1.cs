7616f97ae74b82ed7b15483a5d6d0376c3905c5f
Meliha SAYAR <melihasayar6@gmail.com>
2021-03-05 00:52:55 +0300

    5 Mart 2021

--- Core/Aspects/Autofac/Validation/Class1.cs
+++ Core/Aspects/Autofac/Validation/Class1.cs
@@ -16,7 +16,7 @@ namespace Core.Aspects.Autofac.Validation
         {
             if (!typeof(IValidator).IsAssignableFrom(validatorType))
             {
-                throw new System.Exception(AspectMessages.WrongValidationType);
+                throw new System.Exception("Bu bir doğrulama sınıfı değil");
             }
 
             _validatorType = validatorType;
@@ -25,7 +25,7 @@ namespace Core.Aspects.Autofac.Validation
         {
             var validator = (IValidator)Activator.CreateInstance(_validatorType);
             var entityType = _validatorType.BaseType.GetGenericArguments()[0];
-            var entities = invocation.Arguments.Where(t => t.GetType() == entityType);
+            var entities = invocation.Arguments.Where(t => t.GetType() == entityType);  //invocation -> metot demek
             foreach (var entity in entities)
             {
                 ValidationTool.Validate(validator, entity);
